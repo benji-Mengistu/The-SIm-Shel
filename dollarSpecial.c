07ca585da7f8aea65fb3155121e90e4ad2a5d73d
benjamin <96646092+benji-Mengistu@users.noreply.github.com>
2022-05-20 22:34:56 +0300

    Update and rename _dollar_special.c to dollarSpecial.c

--- dollarSpecial.c
+++ dollarSpecial.c
@@ -2,7 +2,6 @@
 
 static int EXIT_CODE;
 static int no_init_exit_code = 1;
-
 /**
  * process_exit_code - Returns the address at which EXIT_CODE is stored
  *
@@ -15,10 +14,8 @@ int *process_exit_code()
 		EXIT_CODE = 0;
 		no_init_exit_code = 0;
 	}
-
 	return (&EXIT_CODE);
 }
-
 /**
  * set_process_exit_code - Sets the value for EXIT_CODE var
  * @code: Number representing exit code
